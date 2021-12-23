44e6e1df205bf98cf3b19aef1027a1a0fd0935f7
Daniel Paulus <daniel.paulus@icloud.com>
2021-12-23 12:15:17 +0100

    bla

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
-
+console.log("hey")
+console.log("<script>alert('hi')</script>")
 new Vue({
   router,
   render: h => h(App)
